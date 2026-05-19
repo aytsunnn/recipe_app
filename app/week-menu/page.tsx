42160098f4dc11fb2787b8bc185850557daeba12
aytsunnn <lohkinaas@gmail.com>
2026-05-19 16:12:23 +0400

    sborka

--- app/week-menu/page.tsx
+++ app/week-menu/page.tsx
@@ -1,5 +1,6 @@
 ﻿"use client";
 
+import { Suspense } from "react";
 import LeftPart from "../components/MainScreen/NavigationLeftPart";
 import RightPart from "../components/MainScreen/NewsRightPart";
 import WeekMenuContent from "./components/WeekMenuContent";
@@ -8,7 +9,9 @@ export default function WeekMenuPage() {
   return (
     <div className="flex w-full gap-5">
       <div className="hidden w-55.75 lg:flex">
-        <LeftPart />
+        <Suspense fallback={<div className="text-umami-gray">Загрузка...</div>}>
+          <LeftPart />
+        </Suspense>
       </div>
 
       <div className="w-full pb-10 lg:w-169.5">
